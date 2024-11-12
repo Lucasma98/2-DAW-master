c808070dacebd73e023f110131fb971d14888fc1
Lucas <lucasitomz05@gmail.com>
2024-11-12 14:08:19 +0100

    Add HTML, CSS, and JavaScript files for "Atrapa la Bola" game implementation

---
+++ DWEC/Javascript/ej13.atrapaLaBola_conBD/server/getRecords.php
@@ -0,0 +1,42 @@
+<?php
+
+use LDAP\Result;
+
+if(isset($_GET['topn'])) {
+ 
+    class record{
+        var $nombre;
+        var $puntos;
+    }
+    
+    function queryRecords($limite,$conexion) {
+        $consulta = "SELECT nombre, puntos 
+                    FROM records 
+                    ORDER BY puntos DESC 
+                    LIMIT $limite";
+        $sen = $conexion->prepare($consulta);//linea de preparacion
+        $sen->execute();//linea de ejecucion
+        $result = [];
+        while($row = $sen->fetch(PDO::FETCH_NAMED)){
+            $rec = new record();
+            $rec->nombre = $row['nombre'];
+            $rec->puntos = $row['puntos'];
+            $result[] = $rec;
+        }
+        return $result;
+    }
+
+    $server = "mysql:dbname=atrapaLaBola";
+	$user = "root";
+	$pw = "";
+	$con = new PDO($server,$user,$pw,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+    
+    echo json_encode( queryRecords($_GET['topn'],$con) );
+        /*"[
+        
+        
+        ] "*/
+} else {
+	echo "No has pasado los parámetros correctos. Debes pasar 'topn'";
+}
+?>
