e5bdd2043334519e6831c4feb5fcac6b8ad70d93
Lucas <1910898@alu.murciaeduca.es>
2025-01-23 00:06:02 +0100

    Add initial Angular wallet application and Flask backend setup

---
+++ DWEC/Angular/wallet/src/app/market-data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class MarketDataService {
+
+  constructor() {
+    private urlBase = "https://api.coinlore.net./api/"
+    private urlSufix = ""
+  
+
+
+   }
+}
---
+++ DWEC/Angular/wallet/src/app/my-wallet/my-wallet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-wallet',
+  imports: [],
+  templateUrl: './my-wallet.component.html',
+  styles: ``
+})
+export class MyWalletComponent {
+
+}
