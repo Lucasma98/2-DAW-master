8862ac5baf3beb0adb74e2571a6cec31ff012147
Lucas <1910898@alu.murciaeduca.es>
2025-01-23 10:29:39 +0100

    asd

--- DWEC/Angular/3.weather/src/app/cities.service.ts
+++ DWEC/Angular/3.weather/src/app/cities.service.ts
@@ -8,15 +8,20 @@ export class CitiesService {
   private cities:any[] = []
   private cityAtHome:any = {}
 
-  constructor() { }
+  constructor() {
+    this.cities = JSON.parse(localStorage.getItem("cities") || "[]")//si hay una ciudad la recupera, la parsea y la guarda en la variable cities y si no hay ninguna, se guarda un null
+    this.cityAtHome = JSON.parse(localStorage.getItem("cityAtHome") || "{}")
+  }
 
   addCity(newCity:any){
     if ( !this.isAlreadyInMyCities(newCity)){ //comprueba si hay una ciudad con el mismo id que ya has insertado y si no es asi la crea
       this.cities.push(newCity)
+      this.saveAll()
     }
   }
   deleteCity(aCity:any){ //aCity porque borras una ciudad
     this.cities = this.cities.filter( c => c.id != aCity.id)
+    this.saveAll()
   }
 
   getCities() {
@@ -29,10 +34,15 @@ export class CitiesService {
 
   setCityAtHome(aCity:any){
     this.cityAtHome = aCity
+    this.saveAll()
   }
 
   isAlreadyInMyCities(aCity:any){ //aqui comprueba el id
     return this.cities.find (c => c.id == aCity.id)
   }
 
+  saveAll(){
+    localStorage.setItem("cities",JSON.stringify(this.cities))
+    localStorage.setItem("cityAtHome",JSON.stringify(this.cityAtHome))
+  }
 }
--- DWEC/Angular/3.weather/src/app/city-at-home/city-at-home.component.ts
+++ DWEC/Angular/3.weather/src/app/city-at-home/city-at-home.component.ts
@@ -1,11 +1,20 @@
+//necesitas conectar el html del city-at-home(la carta) con el de manage, a traves del servicio cities.service
 import { Component } from '@angular/core';
+import { CitiesService } from '../cities.service';
+import { TempPipe } from '../temp.pipe';
+import { CountryCodePipe } from '../country-code.pipe';
 
 @Component({
   selector: 'app-city-at-home',
-  imports: [],
+  imports: [TempPipe,CountryCodePipe],
   templateUrl: './city-at-home.component.html',
   styles: ``
 })
 export class CityAtHomeComponent {
+  constructor (private citiesSrvc:CitiesService){
 
+  }
+  getCityAtHome(){
+    return this.citiesSrvc.getCityAtHome()
+  }
 }
